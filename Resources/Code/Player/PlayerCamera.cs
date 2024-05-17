fa1861f7934f28b6ca47e44efa2fb1b99db2c54b
adrian-azan <adrian.azan@hotmail.com>
2024-05-17 17:40:37 -0400

    Code Maid Cleanup
    Test looks good so cleaning up other touched files. Holding off on cleaning every file so this PR stays on the smaller side

--- Resources/Code/Player/PlayerCamera.cs
+++ Resources/Code/Player/PlayerCamera.cs
@@ -1,28 +1,26 @@
 using Godot;
-using System;
 
 public partial class PlayerCamera : Node3D
 {
-    AnimationPlayer _animationPlayer;
+    private AnimationPlayer _animationPlayer;
 
-	[Export(PropertyHint.Range, "0,1,0.05")]
-	private float _bobbingWeight = 1.0f;
+    [Export(PropertyHint.Range, "0,1,0.05")]
+    private float _bobbingWeight = 1.0f;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_animationPlayer = GetNode<AnimationPlayer>("Camera3D/AnimationPlayer");
-		_animationPlayer.Play("player_headBob");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _animationPlayer = GetNode<AnimationPlayer>("Camera3D/AnimationPlayer");
+        _animationPlayer.Play("player_headBob");
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 
-
-	public void SetSpeed(float speed)
-	{
-        _animationPlayer.SpeedScale = speed*_bobbingWeight;
+    public void SetSpeed(float speed)
+    {
+        _animationPlayer.SpeedScale = speed * _bobbingWeight;
     }
 }
