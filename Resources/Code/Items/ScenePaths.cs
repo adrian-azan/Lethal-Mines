11f4b845fd8e31d9faf57f62410b7cdaeee7c0fe
adrian-azan <adrian.azan@hotmail.com>
2024-08-15 21:05:40 -0400

    Path changes
    Combined a few path constants just into different sub classes

--- Resources/Code/Items/ScenePaths.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-
-public class ScenePaths
-{
-    public const string PICKAXE = "res://Resources/Scenes/Items/Pickaxe.tscn";
-    public const string DROP = "res://Resources/Scenes/Drop.tscn";
-}
