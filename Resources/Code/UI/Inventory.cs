a962c8cecd0cef7ab38b1a724c1d3327e006f9b4
adrian-azan <adrian.azan@hotmail.com>
2024-09-23 19:03:13 -0400

    Updated Inventory Additions
    Inventory now looks for the stackable item first, and if it can't find the item it will fill to the first empty spot. I refactored to find a slot in any way. I may add options to back fill an inventory, apply a white and blacklist, etc

--- Resources/Code/UI/Inventory.cs
+++ Resources/Code/UI/Inventory.cs
@@ -24,26 +24,62 @@ public partial class Inventory : Control
 
         var physicalItemPath = newItem.Replace("UI_Data", "Objects");
 
-        foreach (var slot in _backpack)
+        Slot potentialSlot;
+
+        potentialSlot = FindStackableSlot(itemName);
+
+        if (potentialSlot != null)
         {
-            //TODO: Whole inventory/Hotbar should be searched for any stackable items
-            //Keep track of first empty slot and put item there if no relavant slots
-            if (slot.IsEmpty() || (!slot.IsEmpty() && (itemName.Contains(slot._item.GetName()) && slot._item._stackable)))
-            {
-                slot.AddItem(newItem);
+            potentialSlot.AddItem(newItem);
+            AddToEquipment(newItem, physicalItemPath);
 
-                if (!_equipment.ContainsKey(newItem))
-                {
-                    var physicalItem = (ResourceLoader.Load(physicalItemPath) as PackedScene).Instantiate() as Item;
-                    AddChild(physicalItem);
-                    _equipment.Add(newItem, physicalItem);
-                }
+            return true;
+        }
 
-                return true;
-            }
+        potentialSlot = FindEmptySlot(itemName);
+        if (potentialSlot != null)
+        {
+            potentialSlot.AddItem(newItem);
+            AddToEquipment(newItem, physicalItemPath);
+
+            return true;
         }
 
         return false;
+    }
+
+    private void AddToEquipment(string newItem, string physicalItemPath)
+    {
+        if (!_equipment.ContainsKey(newItem))
+        {
+            var physicalItem = (ResourceLoader.Load(physicalItemPath) as PackedScene).Instantiate() as Item;
+            AddChild(physicalItem);
+            _equipment.Add(newItem, physicalItem);
+        }
+    }
+
+    private Slot FindStackableSlot(string itemName)
+    {
+        foreach (var slot in _backpack)
+        {
+            if (!slot.IsEmpty() && (itemName.Contains(slot._item.GetName()) && slot._item._stackable))
+            {
+                return slot;
+            }
+        }
+        return null;
+    }
+
+    private Slot FindEmptySlot(string itemName)
+    {
+        foreach (var slot in _backpack)
+        {
+            if (slot.IsEmpty())
+            {
+                return slot;
+            }
+        }
+        return null;
     }
 
     public void Use(Player player)
