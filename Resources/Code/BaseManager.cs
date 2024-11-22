2611650431f5de659894b9fe8cdfe1a0270c5f47
adrian-azan <adrian.azan@hotmail.com>
2024-11-22 11:46:19 -0500

    Minor PR cleanup

--- Resources/Code/BaseManager.cs
+++ Resources/Code/BaseManager.cs
@@ -3,13 +3,11 @@ using System;
 
 public partial class BaseManager : Node
 {
-    private Player _player;
     private Node3D _baseHatch;
     private Node3D _baseSpawn;
 
     public override void _Ready()
     {
-        _player = GetNode<Player>("../");
         _baseHatch = GetNode<Node3D>("BaseHatch");
         _baseSpawn = GetNode<Node3D>("Inside/SPAWN");
     }
